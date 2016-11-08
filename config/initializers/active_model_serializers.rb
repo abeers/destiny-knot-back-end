3abd09f6b9e4d0517cf1e32f3abc0da8f7d85df5
abeers <alexbeers92@gmail.com>
2016-11-08 18:31:20 -0500

    THIS LINE IS NECESSARY AT BUILD
    I was unsure about how necessary this config line was, but it is for
    how the client is set up. Don't comment it out, don't delete it.

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,3 +1,3 @@
 # Root keys on json resources
 ActiveModelSerializers.config.adapter = :json
-# ActiveModelSerializers.config.default_includes = '**'
+ActiveModelSerializers.config.default_includes = '**'
