bad6ec4f3f7434b03dade64167da7896b92ea2c9
abeers <alexbeers92@gmail.com>
2016-11-08 19:49:57 -0500

    Sets the client origin to the deployed front end
    Prepares for deployment

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module DestinyKnotApi
     # Cross-Origin Resource Sharing
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_ORIGIN'] || 'http://localhost:7165'
+        origins ENV['CLIENT_ORIGIN'] || 'https://abeers.github.io'
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
