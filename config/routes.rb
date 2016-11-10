232375d31e3a24970ac581589b99b81c6aff5d39
abeers <alexbeers92@gmail.com>
2016-11-10 14:01:47 -0500

    Only has show and index routes for resources
    Non team and team-member resources no longer have routes. Also sets
    localhost

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   resources :team_members, except: [:new, :edit]
   resources :teams, except: [:new, :edit]
-  resources :type_efficacies, except: [:new, :edit]
-  resources :pokemon_types, except: [:new, :edit]
-  resources :types, except: [:new, :edit]
-  resources :pokemon_abilities, except: [:new, :edit]
-  resources :abilities, except: [:new, :edit]
-  resources :pokemon_stats, except: [:new, :edit]
-  resources :stats, except: [:new, :edit]
-  resources :pokemons, except: [:new, :edit]
+  resources :type_efficacies, only: [:index, :show]
+  resources :pokemon_types, only: [:index, :show]
+  resources :types, only: [:index, :show]
+  resources :pokemon_abilities, only: [:index, :show]
+  resources :abilities, only: [:index, :show]
+  resources :pokemon_stats, only: [:index, :show]
+  resources :stats, only: [:index, :show]
+  resources :pokemons, only: [:index, :show]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
